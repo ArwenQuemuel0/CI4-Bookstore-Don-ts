3376d1440202544a509770e26fd326ac470b60d8
ArwenQuemuel0 <mlquemuel@fit.edu.ph>
2026-03-15 17:43:00 +0800

    Initialize frontend/cartPage

---
+++ backend/app/Controllers/Users.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Controllers\BaseController;
+
+class Users extends BaseController
+{
+    public function index()
+    {
+        return view('user/landingPage');
+    }
+
+    public function login()
+    {
+        return view('user/loginPage');
+    }
+
+    public function signup()
+    {
+        return view('user/signupPage');
+    }
+
+    public function moodboard()
+    {
+        return view('user/moodboardPage');
+    }
+
+    public function roadmap()
+    {
+        return view('user/roadmapPage');
+    }
+
+    public function cart()
+    {
+        $session = session();
+
+        // Redirect if user not logged in
+        if (!$session->has('user')) {
+            return redirect()->to('/loginPage');
+        }
+
+        $cartItems = $session->get('cart') ?? [];
+        $totalPrice = 0;
+
+        foreach ($cartItems as $item) {
+            $totalPrice += $item['price'] * $item['quantity'];
+        }
+
+        return view('user/cartPage', [
+            'cartItems' => $cartItems,
+            'totalPrice' => $totalPrice,
+            'userFirstName' => $session->get('user')['first_name'] ?? 'Reader'
+        ]);
+    }
+}
